a59d70b876f501fbb3c0b9ac3c2775319bd6fe4e
DuyHao20033 <duyhao20033@gmail.com>
2025-10-21 16:18:22 +0700

    Cập nhật tính năng mới

--- Repository/DataContext.cs
+++ Repository/DataContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using QUANLYKHO.Models;
+using StockManagementMVC.Models;
 
 namespace QUANLYKHO.Repository
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) :base(options)
-            {
-            }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProductModel> Products { get; set; }
+
     }
 }
